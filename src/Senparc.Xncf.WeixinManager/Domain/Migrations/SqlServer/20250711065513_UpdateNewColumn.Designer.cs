0e23b02e04cee5f9019b11b56ecc0fb24f76ec32
JeffreySu <zsu@senparc.com>
2025-07-11 14:56:22 +0800

    添加 SQL Server 最新数据库迁移

--- src/Senparc.Xncf.WeixinManager/Domain/Migrations/SqlServer/20250711065513_UpdateNewColumn.Designer.cs
+++ src/Senparc.Xncf.WeixinManager/Domain/Migrations/SqlServer/20250711065513_UpdateNewColumn.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Senparc.Xncf.WeixinManager.Domain.Models.MultipleDatabase;
 
 #nullable disable
 
-namespace Senparc.Xncf.WeixinManager.Domain.Migrations.Migrations.SqlServer
+namespace Senparc.Xncf.WeixinManager.Domain.Migrations.SqlServer
 {
     [DbContext(typeof(WeixinSenparcEntities_SqlServer))]
-    partial class WeixinSenparcEntities_SqlServerModelSnapshot : ModelSnapshot
+    [Migration("20250711065513_UpdateNewColumn")]
+    partial class UpdateNewColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
